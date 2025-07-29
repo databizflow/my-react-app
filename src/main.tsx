fad4031610d4262645475c20e0f8cf978556e95b
valenti <databizflow@gmail.com>
2025-07-29 14:04:05 +0900

    1

--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,10 @@ createRoot(document.getElementById('root')!).render(
     <App />
  
 )
+
+// 사용하지 않고 있음 → 아래에 쓰지 않으면 에러 발생
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
